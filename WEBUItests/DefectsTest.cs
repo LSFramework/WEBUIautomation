d9c81f44a4c0efe0c2180bc5cbeea8e7c5e30622
vetushkin <vetushkin@gmail.com>
2014-06-05 16:33:01 +0300

    DefectsPage mockup

--- WEBUItests/DefectsTest.cs
+++ WEBUItests/DefectsTest.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WEBUIautomation;
 
 namespace WEBUItests
 {
     class DefectsTest
     {
+        [TestMethod]
+        public void Post_Defect()
+        {
+            DefectsPage.GoTo();
+
+            Assert.IsTrue(DefectsPage.IsAt, "failed to login");
+
+            DefectsPage.AddDefect()
+                .EnterSummary("")
+                .SetSeverity("")
+                .Add();
+
+            Assert.AreEqual(DefectsPage.NewDefect, DefectsPage.LastCreatedDefect, "Defect is not created");
+
+        }
     }
 }
