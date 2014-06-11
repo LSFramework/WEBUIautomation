44e0c8c5a5e74c8882c23ea513bf2c440521e8cf
Artem Hrekul <artem.grekul@gmail.com>
2014-06-11 16:42:03 +0300

    final changes after merge

---
+++ WEBUIautomation/Pages/DashboardPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WEBUIautomation
+{
+    public class DashboardPage
+    {
+        public static bool IsAt
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
