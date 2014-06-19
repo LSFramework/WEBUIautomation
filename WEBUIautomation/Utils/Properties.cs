ff81f30fd36e2d0410e9295454faf14796683a2c
vetushkin <vetushkin@gmail.com>
2014-06-19 15:21:55 +0300

    added mockups

---
+++ WEBUIautomation/Utils/Properties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBUIautomation.Utils
+{
+    //Create XML properties file to read from
+    class Properties
+    {
+    }
+}
