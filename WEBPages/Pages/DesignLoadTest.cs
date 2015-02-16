fc8a9cc9e9f32028120753fc7ac11c1d0613473d
semiroz <semiroz@gmail.com>
2015-02-16 10:40:16 +0200

    DLT

---
+++ WEBPages/Pages/DesignLoadTest.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WEBUIautomation;
+using WEBUIautomation.Utils;
+
+namespace WEBPages.Pages
+{
+    public class DesignLoadTest
+    {
+        #region DLT Frames
+
+        public static void SelectWorkloadType()
+        {
+                MyPCNavigation.SwitchToFrame(@"//iframe[contains(@ng-src,'PreManageLoadTest.aspx')]");
+                MyPCNavigation.SwitchToFrame(@"//*[@id='workload')]");
+               // Driver.Instance.FindElementAndWait(By.XPath(@"//*[@id='workload')]"));   
+                OKBtn.Click();
+        }
+
+        public static IWebElement OKBtn
+            {get {return Driver.Instance.FindElementAndWait(By.XPath(".//*[@id='ctl00_PageContent_WorkloadTypeDialog_C_WorkloadCntrl_btnOk']"));} }
+
+        public static IWebElement CancelBtn
+            { get { return Driver.Instance.FindElementAndWait(By.XPath(".//*[@id='ctl00_PageContent_WorkloadTypeDialog_C_WorkloadCntrl_btnCancel'] ")); } }
+   
+        #endregion
+
+    }
+}
